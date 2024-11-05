dfaba40594febcc28b7698db98bff8803e599c6b
xiaolan <songguoqing2001@163.com>
2024-11-05 00:24:41 +0000

    241105#daily save

--- ipc-client/main.cpp
+++ ipc-client/main.cpp
@@ -1,18 +1,11 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include "widget.h"
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
 
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
-
-    return app.exec();
+    return a.exec();
 }
